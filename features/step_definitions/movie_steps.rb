a7d1e3fc105cb630c20ebbbb159c4a3c234023fe
Qiao <qiao-liang@uiowa.edu>
2016-10-05 03:56:29 +0000

    Finished the first 2 parts

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,9 +28,9 @@ Given /^I am on the RottenPotatoes home page$/ do
    click_on "More about #{title}"
  end
 
- Then /^(?:|I )should see "([^"]*)"$/ do |text|
-    expect(page).to have_content(text)
- end
+#  Then /^(?:|I )should see "([^"]*)"$/ do |text|
+#     expect(page).to have_content(text)
+#  end
 
  When /^I have edited the movie "(.*?)" to change the rating to "(.*?)"$/ do |movie, rating|
   click_on "Edit"
@@ -46,12 +46,12 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
     # Each returned movie will be a hash representing one row of the movies_table
     # The keys will be the table headers and the values will be the row contents.
     # Entries can be directly to the database with ActiveRecord methods
     # Add the necessary Active Record call(s) to populate the database.
+    Movie.create(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
   end
 end
 
@@ -59,15 +59,33 @@ When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  rating_list = arg1.split(",").map { |id| id = "ratings_" + id.strip }
+  all('input[type=checkbox]').each do |ckb|
+      if rating_list.include?(ckb[:id])
+          check(ckb[:id])
+      else
+          uncheck(ckb[:id])
+      end
+  end
+  
+  click_button 'Refresh'
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+    result = true
+    rating_list = arg1.split(",").collect(&:strip)
+    find('tbody').all('tr').each do |row|
+        if !rating_list.include?(row.all('td')[1].text)
+            result = false
+            break
+        end
+    end
+    
+    expect(result).to be_truthy 
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+    expect(Movie.count == find('tbody').all('tr').size).to be_truthy
 end
 
 
